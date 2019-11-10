535b978188f0b06764ea2c733b3e90e7c93e5985
donchelli <tim.melcherson@gmail.com>
2019-11-10 17:02:16 +0100

    Heroku deploy version

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	mongoURIAtlas: 'mongodb+srv://tim:zAg!nNuJ33k@portfoliocluster-9cuzx.gcp.mongodb.net/test?retryWrites=true&w=majority',
-	mongoURILocal: 'mongodb://localhost:27017/portfolio'
-};
