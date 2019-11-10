e45693ae8e0de48ae504e520f8c2a1eb6dc9d0dc
donchelli <tim.melcherson@gmail.com>
2019-11-10 19:30:22 +0100

    Changed the db variable initiation

--- server.js
+++ server.js
@@ -18,18 +18,6 @@ app.use('/uploads/', express.static('uploads'));
 // Custom API routes
 app.use('/api/projects', projects);
 
-// DB Config
-// Atlas URI
-const db = require('./config/keys').mongoURIAtlas;
-
-// Connect to Mongo
-mongoose
-	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-	.then(() => console.log('MongoDB Connected'))
-	.catch(err => console.log(err));
-
-// mongoose.set('useFindAndModify', false);
-
 // Set static folder
 app.use(express.static(path.join(__dirname, 'client/build')));
 
@@ -38,16 +26,38 @@ app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
 });
 
+let db;
+
 // Serve static assets if in production (This is for Heroku deployment)
 if (process.env.NODE_ENV === 'production') {
+	// DB Config
+	// Atlas URI
+	db = process.env.DB_CONFIG_KEY;
+	console.log(db);
 	// Set static folder
 	app.use(express.static('client/build'));
 
-	// Catch all requests that doesn't match the above 
+	// Catch all requests that doesn't match the above
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	})
+	});
+} else {
+	// DB Config
+	// Atlas URI
+	db = require('./config/keys').mongoURIAtlas;
 }
+
+// Connect to Mongo
+mongoose
+	.connect(db, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true
+	})
+	.then(() => console.log('MongoDB Connected'))
+	.catch(err => console.log(err));
+
+// mongoose.set('useFindAndModify', false);
 
 // Define a PORT variable, process.env.PORT is for Heroku deployment
 const PORT = process.env.PORT || 5000;
