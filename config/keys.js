8fbab90bed2e0ab293738ee8651106b97dc09aad
donchelli <tim.melcherson@gmail.com>
2019-11-10 16:31:33 +0100

    Trying to deploy again

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+	mongoURIAtlas: 'mongodb+srv://tim:zAg!nNuJ33k@portfoliocluster-9cuzx.gcp.mongodb.net/test?retryWrites=true&w=majority',
+	mongoURILocal: 'mongodb://localhost:27017/portfolio'
+};
